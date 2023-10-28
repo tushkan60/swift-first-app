609b5d6299b02eb54235a4d88580fda47bef967d
Pavel Zadkov <pavelzadkov@MacBook-Pro-Pavel.local>
2023-10-28 16:27:59 +0300

    first commit

---
+++ first-swift-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first-swift-app
+//
+//  Created by Pavel Zadkov on 28.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
